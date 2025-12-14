8667629dc5a9e0889f3d4903c3f2a2cfd48fe3f2
iliksis <40835286+iliksis@users.noreply.github.com.>
2025-12-14 14:17:10 +0100

    Removed react-hook-form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,14 +1,7 @@
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "@tanstack/react-form";
 import { useRouter } from "@tanstack/react-router";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { useMutation } from "@/hooks/useMutation";
 import { loginFn } from "@/routes/_authed";
-
-const formSchema = z.object({
-	userName: z.string().min(1),
-	password: z.string().min(1),
-});
 
 export function Login() {
 	const router = useRouter();
@@ -23,62 +16,75 @@ export function Login() {
 		},
 	});
 
-	const { handleSubmit, register } = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
+	const form = useForm({
 		defaultValues: {
 			userName: "",
 			password: "",
 		},
+		onSubmit: async ({ value }) => {
+			loginMutation.mutate({
+				data: {
+					userName: value.userName,
+					password: value.password,
+				},
+			});
+		},
 	});
-
-	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		loginMutation.mutate({
-			data: {
-				userName: values.userName,
-				password: values.password,
-			},
-		});
-		// const response = await login(values, searchParams.get("redirect"));
-		// if (response) {
-		// 	setError(response);
-		// }
-	};
 
 	return (
 		<div className="w-dvw h-dvh relative">
 			<form
 				className="card w-full max-w-sm bg-base-300 shrink-0 shadow-2xl absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
-				onSubmit={handleSubmit(onSubmit)}
+				onSubmit={(e) => {
+					e.preventDefault();
+					e.stopPropagation();
+					form.handleSubmit();
+				}}
 			>
 				<div className="card-body">
-					<fieldset className="fieldset">
-						{/* register your input into the hook by invoking the "register" function */}
-						<label className="label" htmlFor="userName">
-							User Name
-						</label>
-						<input
-							id="userName"
-							className="input input-primary w-full"
-							placeholder="User Name"
-							{...register("userName", { required: true })}
-						/>
-
-						{/* include validation with required or other standard HTML validation rules */}
-						<label className="label" htmlFor="password">
-							Password
-						</label>
-						<input
-							id="password"
-							className="input input-primary w-full"
-							type="password"
-							placeholder="Password"
-							{...register("password", { required: true })}
-						/>
-
-						<button className="btn btn-primary mt-4" type="submit">
-							Login
-						</button>
-					</fieldset>
+					<form.Field name="userName">
+						{(field) => {
+							return (
+								<fieldset className="fieldset">
+									{/* register your input into the hook by invoking the "register" function */}
+									<label className="label" htmlFor={field.name}>
+										User Name
+									</label>
+									<input
+										id={field.name}
+										className="input input-primary w-full"
+										placeholder="User Name"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+									/>
+								</fieldset>
+							);
+						}}
+					</form.Field>
+					<form.Field name="password">
+						{(field) => {
+							return (
+								<fieldset className="fieldset">
+									{/* register your input into the hook by invoking the "register" function */}
+									<label className="label" htmlFor={field.name}>
+										Password
+									</label>
+									<input
+										id={field.name}
+										className="input input-primary w-full"
+										type="password"
+										placeholder="Password"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+									/>
+								</fieldset>
+							);
+						}}
+					</form.Field>
 				</div>
 			</form>
 		</div>
