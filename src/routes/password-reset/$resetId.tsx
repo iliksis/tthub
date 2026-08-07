daab4910bfd3bc3c6ecf24650457ccfd9a95a3d9
iliksis <40835286+iliksis@users.noreply.github.com.>
2026-08-07 15:26:30 +0200

    feat: enhance password reset and invitation forms with Card component for improved UI

--- src/routes/password-reset/$resetId.tsx
+++ src/routes/password-reset/$resetId.tsx
@@ -3,6 +3,7 @@ import { createFileRoute, useRouter } from "@tanstack/react-router";
 import { getPasswordReset } from "@/api/passwordReset";
 import { updatePasswordFromReset } from "@/api/users";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useMutation } from "@/hooks/useMutation";
@@ -74,79 +75,82 @@ function RouteComponent() {
 
 	return (
 		<div className="w-dvw h-dvh relative">
-			<form
-				className="w-full max-w-sm shrink-0 rounded-xl bg-card text-card-foreground shadow-2xl absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
-				onSubmit={(e) => {
-					e.preventDefault();
-					e.stopPropagation();
-					form.handleSubmit();
-				}}
-			>
-				<div className="flex flex-col gap-2 p-6">
-					<h2 className="text-lg font-semibold leading-none">
-						{t("Update your password")}
-					</h2>
-					<fieldset className="flex flex-col gap-1.5">
-						<Label htmlFor="username">{t("User Name")}:</Label>
-						<Input
-							id="username"
-							name="UserName"
-							disabled={true}
-							value={passwordReset.user.userName}
-						/>
-					</fieldset>
-					<form.Field name="password">
-						{(field) => (
-							<fieldset className="flex flex-col gap-1.5">
-								<Label htmlFor={field.name}>{t("Password")}:</Label>
-								<Input
-									id={field.name}
-									type="password"
-									name={field.name}
-									value={field.state.value}
-									onBlur={field.handleBlur}
-									onChange={(e) => field.handleChange(e.target.value)}
-								/>
-							</fieldset>
+			<Card className="w-full max-w-sm shrink-0 shadow-2xl absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+				<form
+					onSubmit={(e) => {
+						e.preventDefault();
+						e.stopPropagation();
+						form.handleSubmit();
+					}}
+				>
+					<CardContent className="flex flex-col gap-2">
+						<h2 className="text-lg font-semibold leading-none">
+							{t("Update your password")}
+						</h2>
+						<fieldset className="flex flex-col gap-1.5">
+							<Label htmlFor="username">{t("User Name")}:</Label>
+							<Input
+								id="username"
+								name="UserName"
+								disabled={true}
+								value={passwordReset.user.userName}
+							/>
+						</fieldset>
+						<form.Field name="password">
+							{(field) => (
+								<fieldset className="flex flex-col gap-1.5">
+									<Label htmlFor={field.name}>{t("Password")}:</Label>
+									<Input
+										id={field.name}
+										type="password"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+									/>
+								</fieldset>
+							)}
+						</form.Field>
+						<form.Field name="confirmPassword">
+							{(field) => (
+								<fieldset className="flex flex-col gap-1.5">
+									<Label htmlFor={field.name}>{t("Confirm Password")}:</Label>
+									<Input
+										id={field.name}
+										type="password"
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+									/>
+								</fieldset>
+							)}
+						</form.Field>
+						{formErrorMap.onChange && (
+							<div className="text-destructive text-xs">
+								{formErrorMap.onChange}
+							</div>
 						)}
-					</form.Field>
-					<form.Field name="confirmPassword">
-						{(field) => (
-							<fieldset className="flex flex-col gap-1.5">
-								<Label htmlFor={field.name}>{t("Confirm Password")}:</Label>
-								<Input
-									id={field.name}
-									type="password"
-									name={field.name}
-									value={field.state.value}
-									onBlur={field.handleBlur}
-									onChange={(e) => field.handleChange(e.target.value)}
-								/>
-							</fieldset>
-						)}
-					</form.Field>
-					{formErrorMap.onChange && (
-						<div className="text-destructive text-xs">{formErrorMap.onChange}</div>
-					)}
-					<form.Subscribe
-						selector={(state) => [
-							state.canSubmit,
-							state.isSubmitting,
-							state.isDefaultValue,
-						]}
-					>
-						{([canSubmit, isSubmitting, isDefaultValue]) => (
-							<Button
-								type="submit"
-								className="mt-4"
-								disabled={!canSubmit || isDefaultValue}
-							>
-								{isSubmitting ? "..." : t("Update password")}
-							</Button>
-						)}
-					</form.Subscribe>
-				</div>
-			</form>
+						<form.Subscribe
+							selector={(state) => [
+								state.canSubmit,
+								state.isSubmitting,
+								state.isDefaultValue,
+							]}
+						>
+							{([canSubmit, isSubmitting, isDefaultValue]) => (
+								<Button
+									type="submit"
+									className="mt-4"
+									disabled={!canSubmit || isDefaultValue}
+								>
+									{isSubmitting ? t("Loading…") : t("Update password")}
+								</Button>
+							)}
+						</form.Subscribe>
+					</CardContent>
+				</form>
+			</Card>
 		</div>
 	);
 }
